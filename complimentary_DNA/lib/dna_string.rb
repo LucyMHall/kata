c654ca8a4fae01db5b1a2e1e57a48b9687caad3a
LucyMHall <lucymhall1995@gmail.com>
2019-07-27 09:47:24 +0100

    REFACTOR: refactor #create_complimentary_string so that #find_pair is abstracted out

--- complimentary_DNA/lib/dna_string.rb
+++ complimentary_DNA/lib/dna_string.rb
@@ -1,13 +1,18 @@
 class DNA_String
 
   def create_complimentary_string(string)
-    complimentary_pairs = {"A" => "T", "T" => "A", "G" => "C", "C" => "G"}
-    array_of_letters = string.chars
     complimentary_array_of_letters = []
-    array_of_letters.each do |letter|
-      complimentary_array_of_letters << complimentary_pairs[letter]
+    string.chars.each do |letter|
+      complimentary_array_of_letters << find_pair(letter)
     end
     complimentary_array_of_letters.join
+  end
+
+  private
+
+  def find_pair(letter)
+    complimentary_pairs = {"A" => "T", "T" => "A", "G" => "C", "C" => "G"}
+    complimentary_pairs[letter]
   end
 
 end
